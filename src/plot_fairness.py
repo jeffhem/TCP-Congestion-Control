651644af1e72593d938ffd33d71ccbbd1074f27d
Jeff <jeffhem@gmail.com>
2019-04-07 23:06:16 -0500

    first commit

---
+++ src/plot_fairness.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import argparse
+
+parser = argparse.ArgumentParser("Congestion Topo")
+parser.add_argument("cca", help="congestion control algorithm", type=str)
+parser.add_argument("delay", help="propagation delay", type=int)
+args = parser.parse_args()
+delay = args.delay
+cca = args.cca
+
+h1time = []
+h1Data = []
+h2time = []
+h2Data = []
+
+with open('./data/bandwidth/bw_sh1_%s_%s' % (cca, delay)) as csvfile:
+    plots = csv.reader(csvfile, delimiter=' ')
+    for i in range(6):
+        next(plots)
+
+    time = 0
+    for row in plots:
+        h1Data.append(float(row[len(row)-2]))
+        h1time.append(time)
+        time = time + 1
+
+with open('./data/bandwidth/bw_sh2_%s_%s' % (cca, delay)) as csvfile:
+    plots = csv.reader(csvfile, delimiter=' ')
+    for i in range(6):
+        next(plots)
+
+    time = 250
+    for row in plots:
+        h2Data.append(float(row[len(row)-2]))
+        h2time.append(time)
+        time = time + 1
+        
+    
+plt.plot(h1time, h1Data, label="h1")
+plt.plot(h2time, h2Data, label="h2")
+plt.ylabel('bandwidth(Mbps)')
+plt.xlabel('time(s)')
+plt.title('Bandwidth fairness %s w/ %sms delay' % (delay, cca))
+plt.rc('lines', linewidth=1)
+axes = plt.gca()
+axes.set_ylim([0,400])
+plt.legend()
+plt.grid(True)
+plt.show()
---
+++ src/plot_tcpprobe.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import argparse
+
+parser = argparse.ArgumentParser("Congestion Topo")
+parser.add_argument("cca", help="congestion control algorithm", type=str)
+parser.add_argument("delay", help="propagation delay", type=int)
+args = parser.parse_args()
+delay = args.delay
+cca = args.cca
+
+h1time = []
+h1Data = []
+h2time = []
+h2Data = []
+yrange = 1500
+
+if (delay == 81):
+    yrange = 4800
+if (delay == 162):
+    yrange = 9500
+
+with open('./data/tcpprobe/data_%s_%s' % (cca, delay)) as csvfile:
+    plots = csv.reader(csvfile, delimiter=' ')
+    for row in plots:
+        if(row[2] == '10.0.0.1:7979'):
+            h1Data.append(int(row[6]))
+            h1time.append(float(row[0]))
+        if(row[2] == '10.0.0.2:7979'):
+            h2Data.append(int(row[6]))
+            h2time.append(float(row[0]))
+
+    
+plt.plot(h1time, h1Data, label="h1 to s1")
+plt.plot(h2time, h2Data, label="h2 to s2")
+plt.ylabel('cwnd(mss)')
+plt.xlabel('time(s)')
+plt.title('iperf test %s w/ %sms delay' % (delay, cca))
+plt.rc('lines', linewidth=1)
+axes = plt.gca()
+axes.set_ylim([0,yrange])
+plt.legend()
+plt.grid(True)
+plt.show()
